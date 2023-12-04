e38ef6fcb777fb41222f84a3c84f4b3020d3458f
maikCyphlock <maikolaguilar656@gmail.com>
2023-12-04 13:24:24 -0400

    🎉 [ADD] api for fetch data from adventjs.

---
+++ src/app/api/route.js
@@ -0,0 +1,26 @@
+import * as cheerio  from "cheerio";
+export async function GET() {
+    const res = await fetch('https://adventjs.dev/en#retos/...')
+    const html = await res.text()
+    const $ = cheerio.load(html);
+        const retos = []
+       $('a[href^="/challenges/2023"]').each((i, element) => {
+        const text = $(element).text();
+            retos[i] = {
+                day: text
+            }
+       });
+       
+       $('[alt="Challenge\'s image"]')
+       .each((i, element) => {
+        const src = 'https://adventjs.dev'+element.attribs.src
+        console.log();
+        retos[i] = {
+            ...retos[i],
+            src
+        }
+       });
+       
+       
+    return Response.json({ retos })
+  }
